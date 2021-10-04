ba802ea388e6a85d143c8a7691794e9f40fbcfd7
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-10-04 12:32:19 +0100

    all project page complete

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,46 @@ import React from "react"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const ProjectsPage = () => {
+const ProjectsPage = ({
+  data: {
+    allStrapiProject: { nodes: projects },
+  },
+}) => {
+  console.log(projects)
   return (
     <>
-      <h2>projects page</h2>
+      {/* <Seo title="projects"> */}
+      <main>
+        <section className="projects-page">
+          <Projects title="all projects" projects={projects} />
+        </section>
+      </main>
+      {/* </Seo> */}
     </>
   )
 }
+export const query = graphql`
+  {
+    allStrapiProject {
+      totalCount
+      nodes {
+        featured
+        github
+        id
+        description
+        title
+        Image {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
