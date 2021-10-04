e4c56f26d904df1ef678d705e49e2d980f3004fa
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-10-04 11:59:40 +0100

    projects page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,40 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Layout from "../components/layout"
 import Seo from "../components/Seo"
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data)
+  const {
+    allStrapiProject: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} showLink title="featured projects" />
     </Layout>
   )
 }
-
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        Image {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 export default IndexPage
